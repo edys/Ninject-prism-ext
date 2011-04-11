2330a3b5cdd6c16f5d3ae9f412e93c491d1d9f59
edys <rhys.walden@gmail.com>
2011-04-11 20:40:03 +1200

    oops

---
+++ NinjectPrismExt/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.Practices.Prism.Logging;
+using Ninject;
+using Ninject.Parameters;
+using NinjectPrismExt.Properties;
+
+namespace NinjectPrismExt
+{
+    public static  class  KernelExtensions
+    {
+        public static bool IsRegistered<TServiceType>(this IKernel currentKernel)
+        {
+            return currentKernel.CanResolve(currentKernel.CreateRequest(typeof (TServiceType), meta => true, new List<IParameter>(), false, false));             
+        }
+
+
+        public static void  RegisterTypeIfMissing<TFromType, TToType>(this IKernel currentKernel, bool registerAsSingleton) where TToType : TFromType
+        {
+           
+            if (currentKernel.IsRegistered<TFromType>())
+            {
+
+                var logger= currentKernel.Get<ILoggerFacade>();
+                logger.Log(String.Format(CultureInfo.CurrentCulture,Resources.TypeMappingAlreadyRegistered, typeof(TFromType).Name), Category.Debug, Priority.Low);
+            }
+            else
+            {
+                if (registerAsSingleton)
+                {
+                    currentKernel.Bind<TFromType>().To<TToType>().InSingletonScope();
+                }
+                else
+                {
+                    currentKernel.Bind<TFromType>().To<TToType>().InTransientScope();
+
+                }
+            }
+        }
+
+    }
+}
